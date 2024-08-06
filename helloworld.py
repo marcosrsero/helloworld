ea25c190caa8b785ad3683a0729baf280d874e8c
marcosrsero <marcos.rosero@carmeloteresiano.com>
2024-08-06 23:58:01 +0000

    Hell no world

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+ladoa = int(input("Porfavor introduce el lado A: "))
+ladob = int(input("Porfavor introduce el lado B"))
+
+perimetro = ladoa * ladob
+
+print("El perimetro del cuadrado es: " , perimetro)
+
+
